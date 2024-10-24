cf7acfc5f1c78d9f592a8f4e53dfba0c70ef5b08
Aniele Camily Souza Silva <20211204010041@ifrn.local>
2024-10-24 14:31:17 -0300

    Configurações do properties

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -3,11 +3,30 @@ package ifrn.pi.eventos.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ifrn.pi.eventos.models.Evento;
+
 @Controller
 public class EventosController {
 
 	@RequestMapping("/eventos/form")
 	public String form() {
-		return "formEvento";
+		return "formEvento";	
+		
 	}
+	
+	@RequestMapping("/eventos/submit") 
+	public String submit(Evento evento ) {
+		System.out.println("tudo ok ");
+		System.out.println(evento.getNome());
+		System.out.println(evento.getLocal());
+		System.out.println(evento.getData());
+		System.out.println(evento.getHorario());
+		return "formSubmit";
+		
+	
+	}
+	
+		
+	
+	
 }
